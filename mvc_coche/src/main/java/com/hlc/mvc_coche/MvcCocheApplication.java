4142b2ff3c6560d2ef09374ba1d146b8b6b473c7
EscribanoC <escribano.flores.carlos@iescamas.es>
2025-01-09 09:19:31 +0100

    hola

---
+++ mvc_coche/src/main/java/com/hlc/mvc_coche/MvcCocheApplication.java
@@ -0,0 +1,13 @@
+package com.hlc.mvc_coche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCocheApplication.class, args);
+	}
+
+}
---
+++ mvc_coche/src/test/java/com/hlc/mvc_coche/MvcCocheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlc.mvc_coche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCocheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
