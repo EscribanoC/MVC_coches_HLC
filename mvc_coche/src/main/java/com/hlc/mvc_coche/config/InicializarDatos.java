fab7e824d7ded8b785af23c66c0c93d75bc8c2d4
EscribanoC <escribano.flores.carlos@iescamas.es>
2025-01-10 09:19:46 +0100

    cambios InicializarDatos

--- mvc_coche/src/main/java/com/hlc/mvc_coche/config/InicializarDatos.java
+++ mvc_coche/src/main/java/com/hlc/mvc_coche/config/InicializarDatos.java
@@ -4,31 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.hlc.mvc_coche.entidades.Coche;
+import com.hlc.mvc_coche.repositorio.CocheRepositorio;
 import com.hlc.mvc_coche.servicio.CocheServicio;
 
 import jakarta.annotation.PostConstruct;
 
 @Component
 public class InicializarDatos {
-	@Autowired
-	private CocheServicio cocheServicio;
+	
+	private CocheRepositorio cocheRepositorio;
+	public InicializarDatos(CocheRepositorio cocheRepositorio) {
+		this.cocheRepositorio = cocheRepositorio;
+	}
+	
 	
 	@PostConstruct
 	public void init() {
-		if(cocheServicio.listarCoches().iterator().hasNext()) {
+		/*if(cocheRepositorio.findAll().iterator().hasNext()) {
 			return;//Si existen datos en una base de datos, no crea los productos de abajo
-		}
+		}*/
 		
 		Coche coche1 = new Coche();
 		coche1.setMarca("Honda");
 		coche1.setMatricula("0860DLC");
 		coche1.setColor("Gris");
-		cocheServicio.guardarCoche(coche1);
+		cocheRepositorio.save(coche1);
 		
 		Coche coche2 = new Coche();
 		coche2.setMarca("Toyota");
 		coche2.setMatricula("4212ERT");
 		coche2.setColor("Azul");
-		cocheServicio.guardarCoche(coche2);
+		cocheRepositorio.save(coche2);
 	}
 }
